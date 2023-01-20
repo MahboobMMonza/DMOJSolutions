6ea6d31cfb749069f0b4c6ca2af5e68223301fc3
Mohammad Mahdi Mahboob <85646892+habibiLaFerrari97@users.noreply.github.com>
2023-01-20 12:27:50 -0500

    Added DP Contest AtCoder B - Frog 2 (dpb)

---
+++ Solutions/dpb.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define forae(i, a, b, c) for (int i = (a); i <= (b); i += (c))
+#define ford(i, a, b, c) for (int i = (a); i > (b); i -= (c))
+#define forde(i, a, b, c) for (int i = (a); i >= (b); i -= (c))
+#define foral(i, a, b, c) for (long long i = (a); i < (b); i += (c))
+#define forael(i, a, b, c) for (long long i = (a); i <= (b); i += (c))
+#define fordl(i, a, b, c) for (long long i = (a); i > (b); i -= (c))
+#define fordel(i, a, b, c) for (long long i = (a); i >= (b); i -= (c))
+#define MAXN (int) (2e5+5)
+
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+ll h[MAXN], dp[MAXN];
+
+int main() {
+    fio;
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> h[i];
+        dp[i] = LONG_LONG_MAX;
+    }
+    dp[0] = 0;
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j <= k && i - j >= 0; j++) {
+            dp[i] = min(dp[i], abs(h[i] - h[i - j]) + dp[i - j]);
+        }
+    }
+    cout << dp[n - 1];
+    return 0;
+}
