5080174d2b64dcca67e711226820b5b6b2d2eb92
Mohammad Mahdi Mahboob <85646892+habibiLaFerrari97@users.noreply.github.com>
2023-04-27 17:10:32 -0400

    CCC 09 S4 updated faster accepted solution

--- Solutions/ccc09s4.cpp
+++ Solutions/ccc09s4.cpp
@@ -1,47 +1,109 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-unordered_set<int> vis, st;
-int ci[5001] = {0}, sp[5001] = {0}, e[5001][5001] = {0};
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+ll costs[5003];
+vector<pi> adj[5003];
+bool vis[5003];
+
+void spfa(int n, int dest) {
+    int cur;
+    costs[n] = 0;
+    deque<int> q;
+    q.pb(n);
+    vis[n] = 1;
+    while (!q.empty()) {
+        cur = q.fr;
+        q.ppf;
+        vis[cur] = 0;
+        for (size_t i = 0; i < adj[cur].size(); ++i) {
+            if (costs[adj[cur][i].fs] > costs[cur] + (ll) adj[cur][i].sc) {
+                costs[adj[cur][i].fs] = costs[cur] + (ll) adj[cur][i].sc;
+                if (!vis[adj[cur][i].fs]) {
+                    (q.empty() || costs[q.fr] > costs[adj[cur][i].fs]) ? q.pf(adj[cur][i].fs) :
+                    q.pb(adj[cur][i].fs);
+                    vis[adj[cur][i].fs] = 1;
+                }
+            }
+        }
+    }
+}
 
 int main() {
-    int n, t, x, y, c, k, d, mp = INT_MAX;
-    scanf("%d%d", &n, &t);
+    fio;
+    fill(costs, costs + 5001, LONG_LONG_MAX);
+    fill(vis, vis + 5001, false);
+    int n, t, k, x, y, d;
+    int c;
+    cin >> n >> t;
     while (t--) {
-        scanf("%d%d%d", &x, &y, &c);
-        if (x && y && x <= n && y <= n) e[x][y] = e[y][x] = c;
+        cin >> x >> y >> c;
+        adj[x - 1].emplace_back(y - 1, c);
+        adj[y - 1].emplace_back(x - 1, c);
     }
-    scanf("%d", &k);
+    cin >> k;
     while (k--) {
-        scanf("%d%d", &x, &c);
-        sp[x] = c;
-        st.insert(x);
+        cin >> y >> c;
+        adj[n].emplace_back(y - 1, c);
     }
-    scanf("%d", &d);
-    ci[d] = 0;
-    x = d;
-    while (!st.empty()) {
-        if (vis.count(x)) {
-            y = INT_MAX;
-            for (int i = 1; i <= n; i++) {
-                if (ci[i] && ci[i] < y && !vis.count(i)) {
-                    y = ci[i];
-                    x = i;
-                } else if (ci[i] == y && !vis.count(i) && sp[i] < sp[x]) x = i;
-            }
-        }
-        vis.insert(x);
-        if (st.count(x)) {
-            mp = min(mp, ci[x] + sp[x]);
-        }
-        st.erase(x);
-        if (st.empty()) break;
-        for (int i = 1; i <= n; i++) {
-            if (e[x][i] && !vis.count(i)) {
-                ci[i] = (ci[i]) ? min(ci[i], ci[x] + e[x][i]) : ci[x] + e[x][i];
-            }
-        }
-    }
-    printf("%d", mp);
-    return 0;
+    cin >> d;
+    d--;
+    spfa(n, d);
+    cout << costs[d] << edl;
 }
